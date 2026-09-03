691e30d63e1ad2a0660c3109e20a95a6b09a3209
Giovanni Farina <giovanni.farina@zucchetti.com>
2026-09-03 10:56:40 +0200

    GetIncidents

---
+++ node_modules/api-tests/tests/Portal/Products/GetIncidents.spec.ts
@@ -0,0 +1,38 @@
+import { test, expect } from '@playwright/test';
+import { portalClient } from '../../../clients/portal.client';
+import parameters from '../../../data/parameters.json';
+
+test('Research Incidents by ProductCode and Release', async () => {
+  const api = await portalClient();
+
+  const account = parameters.magoPortal.account;
+  const productCode = parameters.magoPortal.productCode;
+  const authToken = parameters.magoPortal.authToken;
+
+  const res = await api.post(
+    `be/api/getIncidents/${account}` + 
+      `?productCode=${encodeURIComponent(productCode)}` +
+      `&releaseFrom=4.2` +
+      `&releaseTo=4.2`,
+    {
+      data: {
+        authToken
+      }
+    }
+  );
+
+  expect(res.status()).toBe(200);
+
+  const body = await res.json();
+
+  console.log('REQUEST PARAMETERS:', {
+    account,
+    productCode,
+    releaseFrom: '4.2',
+    releaseTo: '4.2'
+  });
+
+  console.log(body);
+
+  await api.dispose();
+});
