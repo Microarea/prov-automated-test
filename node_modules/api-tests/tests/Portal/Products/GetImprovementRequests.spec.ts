83e49c0a181433eb83aaa2746f1e0f636d182a9f
Giovanni Farina <giovanni.farina@zucchetti.com>
2026-09-07 10:38:25 +0200

    GetImprovementRequests

--- node_modules/api-tests/tests/Portal/Products/GetImprovementRequests.spec.ts
+++ node_modules/api-tests/tests/Portal/Products/GetImprovementRequests.spec.ts
@@ -2,17 +2,21 @@ import { test, expect } from '@playwright/test';
 import { portalClient } from '../../../clients/portal.client';
 import parameters from '../../../data/parameters.json';
 
-test('Research project improvement by ProductCode and searchString', async () => {
+test('Get Improvement Requests', async () => {
   const api = await portalClient();
 
   const account = parameters.magoPortal.account;
   const productCode = parameters.magoPortal.productCode;
-  const searchString = parameters.magoPortal.searchString1;
+  const companyCode = parameters.magoPortal.companyCode;
+  const status = parameters.magoPortal.status;
+  const searchString = parameters.magoPortal.searchString2;
   const ProxyAuthToken = parameters.magoPortal.ProxyAuthToken;
 
   const res = await api.post(
-    `be/api/getProjectImprovementReleases/${account}` +
+    `be/api/getImprovementRequests/${account}` + 
       `?productCode=${encodeURIComponent(productCode)}` +
+      `&companyCode=${encodeURIComponent(companyCode)}` +
+      `&status=${encodeURIComponent(status)}` +
       `&searchString=${encodeURIComponent(searchString)}`,
     {
       data: {
@@ -28,6 +32,8 @@ test('Research project improvement by ProductCode and searchString', async () =>
   console.log('REQUEST PARAMETERS:', {
     account,
     productCode,
+    companyCode,
+    status,
     searchString
   });
 
