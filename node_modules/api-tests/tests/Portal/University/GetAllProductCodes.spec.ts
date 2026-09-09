2412f400d5fc46ca62e4d9226dc1b7a88db378ae
Giovanni Farina <giovanni.farina@zucchetti.com>
2026-09-09 15:29:24 +0200

    GetAllProductCodes

---
+++ node_modules/api-tests/tests/Portal/University/GetAllProductCodes.spec.ts
@@ -0,0 +1,17 @@
+import { test, expect } from '@playwright/test';
+import { portalClient } from '../../../clients/portal.client';
+import parameters from '../../../data/parameters.json';
+
+test('POST All MagoUniversity Product codes returns 200', async () => {
+  const api = await portalClient();
+  
+  const user =  process.env.GWAM_USER;
+  const res = await api.post(
+    `be/api/university/getAllProductCodes/${user}`
+  );
+
+  expect(res.status()).toBe(200);
+  console.log(await res.json());
+
+  await api.dispose();
+});
