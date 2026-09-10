cd9aa471a5e252ecc3bb14ab26197b2fdbd2653f
Giovanni Farina <giovanni.farina@zucchetti.com>
2026-09-10 16:03:17 +0200

    Blog - GetBlogAccount

---
+++ node_modules/api-tests/tests/Portal/Blog/GetBlogAccount.spec.ts
@@ -0,0 +1,33 @@
+import { test, expect } from '@playwright/test';
+import { portalClient } from '../../../clients/portal.client';
+import parameters from '../../../data/parameters.json';
+
+test.describe('Portal > Blog', () => {
+  test('Get Blog Account', async () => {
+    const api = await portalClient();
+
+  //l'utente autorizzato a fare la chiamata è quello settato nelle variabili d'ambiente
+    const user = process.env.GWAM_USER;
+    const companycode = parameters.GetBlogAccount.companyCode;
+    const email = parameters.GetBlogAccount.email;
+    
+    const res = await api.post(
+      `be/api/getBlogAccount/${user}` + 
+        `?companyCode=${companycode}` + 
+        `&email=${email}`
+    );
+    const body = await res.json();
+
+    console.log('REQUEST PARAMETERS:', {
+      email,
+      companyCode: companycode,
+    });
+    console.log(body);
+
+    expect(res.status()).toBe(200);
+    expect(body.Content).toBeDefined();
+    expect(body.Content?.Email).toBe(user);
+
+    await api.dispose();
+  });
+});
