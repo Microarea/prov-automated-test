60a2cc7189897ba1b11419f01b336db33f42ca18
Giovanni Farina <giovanni.farina@zucchetti.com>
2026-09-10 16:11:31 +0200

    Roles - GetAllRolesForDownload

--- node_modules/api-tests/tests/Portal/Roles/GetAllRolesForDownload.spec.ts
+++ node_modules/api-tests/tests/Portal/Roles/GetAllRolesForDownload.spec.ts
@@ -1,15 +1,15 @@
 import { test, expect } from '@playwright/test';
 import { portalClient } from '../../../clients/portal.client';
 
-test.describe('Portal > Blog', () => {
-  test('Get Account Email', async () => {
+test.describe('Portal > Roles', () => {
+  test('Get All Roles for Download', async () => {
     const api = await portalClient();
 
   //l'utente autorizzato a fare la chiamata è quello settato nelle variabili d'ambiente
     const user = process.env.GWAM_USER;
     
     const res = await api.post(
-      `be/api/getAccountEmail/${user}`
+      `be/api/getAllRolesForDownload/${user}`
     );
     const body = await res.json();
 
@@ -17,7 +17,6 @@ test.describe('Portal > Blog', () => {
 
     expect(res.status()).toBe(200);
     expect(body.Content).toBeDefined();
-    expect(body.Content).toBe(user);
 
     await api.dispose();
   });
